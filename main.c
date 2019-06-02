7094f014a90cd886533f44799c4db0146153a1e9
Den095 <51302681+Den095@users.noreply.github.com>
2019-06-03 02:59:50 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+// this is training program
+#include <std.h>
+int main()
+{	
+	printf("Enter 2 num, please \n");
+	scanf("%d%d", &a, &b);
+	printf("\nSumm of a and b = %d\n", (a+b));
+	printf("\nSubtraction of a and b = %d\n", (a-b));
+	printf("\nMultiplication of a and b = %d\n", (a*b));
+	printf("\nDivision of a and b = %d\n", (a/b));
+	return 0;
+}
